78928904d9c68d92a3e3017248df03cec68f25b1
Parth Suthar <newcoder13082002@gmail.com>
2023-03-29 12:23:14 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Business from './components/Buisness';
 import Cricket from './components/Cricket';
 import Science from './components/Science';
 import Health from './components/Health';
+import General from './components/General';
 function App() {
   return (
     <>
     < div className='cls' style={{background:"#343a40"}}>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
       
       {/* <Business/> */}
       {/* <Cricket/> */}
       {/* <Science/> */}
       {/* <Health/> */}
+      <General/>
       <footer   style={{background:"#212529",color:"white",textAlign:"center" , height:"50px",margin:"10px 0px" ,bottom:"0"}}>
         <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"50px"}}>This is Footer.</div>
       </footer>
